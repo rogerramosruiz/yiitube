b0998fa84ee138f87a25c8286c4bf5ca5baa2d87
Roger <rramos@bago.com.bo>
2025-06-09 11:39:59 -0400

    create video view

--- backend/views/video/_form.php
+++ backend/views/video/_form.php
@@ -1,7 +1,7 @@
 <?php
 
 use yii\helpers\Html;
-use yii\widgets\ActiveForm;
+use yii\bootstrap5\ActiveForm;
 
 /** @var yii\web\View $this */
 /** @var common\models\Video $model */
--- backend/views/video/create.php
+++ backend/views/video/create.php
@@ -12,9 +12,23 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="video-create">
 
     <h1><?= Html::encode($this->title) ?></h1>
+    
+    <div>
+        <div class="upload-icon">
+            <i class="fa-solid fa-upload"></i>
+        </div>
+        <p>
+            Drag and drop a file you want to upload
+        </p>
 
-    <?= $this->render('_form', [
-        'model' => $model,
-    ]) ?>
+        <p class="text-muted">
+            Your video will be private until publish
+        </p>
+
+        <button class="btn btn-primary btn-file">
+            Select File
+            <input type="file" id="videoFile" name="video">
+        </button>
+    </div>
 
 </div>
