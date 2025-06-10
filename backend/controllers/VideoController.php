740bc5caf37876331b1bd12bcedd91d3ebd8d1a8
Roger <rramos@bago.com.bo>
2025-06-10 09:50:18 -0400

    access control

--- backend/controllers/VideoController.php
+++ backend/controllers/VideoController.php
@@ -4,6 +4,7 @@ namespace backend\controllers;
 
 use common\models\Video;
 use yii\data\ActiveDataProvider;
+use yii\filters\AccessControl;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
@@ -22,6 +23,16 @@ class VideoController extends Controller
         return array_merge(
             parent::behaviors(),
             [
+                'access' => [
+                    'class' => AccessControl::class,
+                    'rules' => [
+                        [
+                            'allow'=> true,
+                            'roles' =>  ['@']
+                        ]
+                    ]
+
+                ],
                 'verbs' => [
                     'class' => VerbFilter::className(),
                     'actions' => [
